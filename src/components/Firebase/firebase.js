66eb74a976040ecda1bd619a7ae4b83d46ca7414
saman darabi <saman.darabi@alopeyk.com>
2020-03-30 21:28:43 +0430

    create admin page

--- src/components/Firebase/firebase.js
+++ src/components/Firebase/firebase.js
@@ -1,5 +1,6 @@
 import app from "firebase/app";
 import "firebase/auth";
+import "firebase/database";
 
 const config = {
   apiKey: "AIzaSyB_d1iPxLD7vHdSKIovzBeaQuLh3XEKrtE",
@@ -15,6 +16,7 @@ class Firebase {
   constructor() {
     app.initializeApp(config);
     this.auth = app.auth();
+    this.db = app.database();
   }
   // *** Auth API ***
   doCreateUserWithEmailAndPassword = (email, password) =>
@@ -28,5 +30,9 @@ class Firebase {
   doPasswordReset = email => this.auth.sendPasswordResetEmail(email);
 
   doPasswordUpdate = password => this.auth.currentUser.updatePassword(password);
+
+  // *** User API ***
+  user = uid => this.db.ref(`users/${uid}`);
+  users = () => this.db.ref("users");
 }
 export default Firebase;
